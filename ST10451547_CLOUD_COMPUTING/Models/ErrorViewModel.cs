cd19937925c844a14aed0c24a066d5df3e5a39e5
KeabetsweDiana <diana.k.mosehla@gmail.com>
2024-04-24 16:51:46 +0200

    First change

---
+++ ST10451547_CLOUD_COMPUTING/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10451547_CLOUD_COMPUTING.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
